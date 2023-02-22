f0d276a42f6ad132c702aefab31501fa7a313918
Benedikt Straube <benediktstraube@hotmail.de>
2023-02-22 18:31:35 +0100

    feat: Lösung für Teil 2

--- src/steps/calculator.steps.ts
+++ src/steps/calculator.steps.ts
@@ -7,14 +7,14 @@ Given('ein Taschenrechner', async function (this: ICustomWorld) {
   await page?.goto('https://testsheepnz.github.io/BasicCalculator.html');
 });
 
-When('ich als erste Zahl drei eingebe', async function (this: ICustomWorld) {
+When('ich als erste Zahl {} eingebe', async function (this: ICustomWorld, firstNumber: string) {
   const page = this.page!;
-  await page.locator('input#number1Field').type('3');
+  await page.locator('input#number1Field').type(firstNumber.toString());
 });
 
-When('ich als zweite Zahl vier eingebe', async function (this: ICustomWorld) {
+When('ich als zweite Zahl {} eingebe', async function (this: ICustomWorld, secondNumber: string) {
   const page = this.page!;
-  await page.locator('input#number2Field').type('4');
+  await page.locator('input#number2Field').type(secondNumber);
 });
 
 When('ich Addition als Operation auswähle', async function (this: ICustomWorld) {
@@ -27,8 +27,8 @@ When('ich das Ergebnis berechne', async function (this: ICustomWorld) {
   await page.locator('input#calculateButton').click();
 });
 
-Then('erwarte ich sieben als Ergebnis', async function (this: ICustomWorld) {
+Then('erwarte ich {} als Ergebnis', async function (this: ICustomWorld, expectedResult: string) {
   const page = this.page!;
   const actualText = await page.locator('#numberAnswerField').inputValue();
-  expect(actualText).toEqual('7');
+  expect(actualText).toEqual(expectedResult);
 });
